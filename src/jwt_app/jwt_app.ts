79476977610ba991eff5ebe0571f022e54b8676d
Vamsi Bellam <bellamvamsikrishna3415@gmail.com>
2024-03-07 21:08:56 -0500

    Use middleware

--- src/jwt_app/jwt_app.ts
+++ src/jwt_app/jwt_app.ts
@@ -1,13 +1,24 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import bodyParser from "body-parser";
 
 const port = 3000;
 let solution = "";
 const app = express();
 
-app.use(bodyParser.text());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.text());
+
+app.use(function (req, res, next) {
+  req.rawBody = "";
+  req.setEncoding("utf8");
+
+  req.on("data", function (chunk) {
+    req.rawBody += chunk;
+  });
+
+  req.on("end", function () {
+    next();
+  });
+});
 
 let secret = "";
 
@@ -17,8 +28,8 @@ app.get("/", (req, res) => {
 
 app.post("/setjwt", (req, res) => {
   try {
-    console.log(req.body);
-    secret = req.body;
+    console.log(req.rawBody);
+    secret = req.rawBody;
     res.send("JWT SECRET set successfully!");
   } catch (error) {
     console.log(error);
@@ -28,7 +39,7 @@ app.post("/setjwt", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   try {
-    const decodedData = jwt.verify(req.body, secret) as jwt.JwtPayload;
+    const decodedData = jwt.verify(req.rawBody, secret) as jwt.JwtPayload;
     if (decodedData?.append) {
       solution = solution + decodedData.append;
       console.log("solution: ", solution);
@@ -37,7 +48,7 @@ app.post("/", (req, res) => {
       res.json({ solution });
     }
   } catch (error) {
-    console.log("IN ERROR: Request Body ", req.body);
+    console.log("IN ERROR: Request Body ", req.rawBody);
     console.log(error);
     res.send("Didn't found token in body");
   }
